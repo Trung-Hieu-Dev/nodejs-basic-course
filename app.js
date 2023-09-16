7191c36022d533ee29f4d656ea06476c681a3c3b
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-09-16 10:02:20 +0700

    Routing request

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-const http = require('http')
+const http = require("http");
 
 // create a local server
 const server = http.createServer((req, res) => {
-  console.log(req)
-})
+  // Sending request
+  const url = req.url;
+  /// routing request
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message' /><button type='submit'>Send</button></form></body>",
+    );
+    res.write("</html>");
+    return res.end();
+  }
 
-server.listen(3000)
+  // Sending response
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello NodeJS</h1></body>");
+  res.write("</html>");
+  res.end();
+});
 
-
-
+server.listen(3000);
