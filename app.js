0731f55aa1d06306b2d2d986351539b798aaca90
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-09-16 10:15:12 +0700

    Redirecting Route

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const http = require("http");
+const fs = require("fs"); // working with file
 
 // create a local server
 const server = http.createServer((req, res) => {
   // Sending request
   const url = req.url;
+  const method = req.method;
+
   /// routing request
   if (url === "/") {
     res.write("<html>");
@@ -12,6 +15,13 @@ const server = http.createServer((req, res) => {
       "<body><form action='/message' method='POST'><input type='text' name='message' /><button type='submit'>Send</button></form></body>",
     );
     res.write("</html>");
+    return res.end();
+  }
+  /// Redirecting
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
     return res.end();
   }
 
