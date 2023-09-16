ccc956cd935ea09abad4c1c12dae19e7550c0216
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-09-16 10:56:48 +0700

    Blocking and Non-Blocking Code

--- app.js
+++ app.js
@@ -25,18 +25,19 @@ const server = http.createServer((req, res) => {
       console.log(chunk);
       body.push(chunk);
     });
-    req.on("end", () => {
+    return req.on("end", () => {
       /// parsing body requests
       const parseBody = Buffer.concat(body).toString(); /// output: message=sth
       /// get requests content
       const message = parseBody.split("=")[1];
       /// writing file
-      fs.writeFileSync("message.txt", message);
+      // fs.writeFileSync("message.txt", message);
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
     });
-
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
   }
 
   // Sending response
