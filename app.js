3703316b198503d713e46dc0a99a60399a7b99a7
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-09-18 11:59:15 +0700

    Adding a 404 Error Page

--- app.js
+++ app.js
@@ -12,4 +12,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+// 404 page
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page Not Found!</h1>");
+});
+
 app.listen(3000);
