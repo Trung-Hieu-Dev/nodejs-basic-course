6ac1af898af18d8aad1ee5f5e3b69ccf04d171b0
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-09-18 11:53:57 +0700

    Handling Routes with Express Router

--- app.js
+++ app.js
@@ -1,24 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+// using express router
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Send</button></form>',
-  );
-});
-
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  console.log("In the middleware");
-  res.send("<h1>Hello from Express</h1>");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
