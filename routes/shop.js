f3c74af72a477cac8e8dd6419ed4a2b6cb537406
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-09-19 11:26:36 +0700

    Outputting Dynamic Content

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,9 @@ const rootDir = require("../utils/path");
 router.get("/", (req, res, next) => {
   // console.log(adminData.products);
   // res.sendFile(path.join(rootDir, "views", "shop.html"));
-  res.render("shop"); // render template
+
+  const products = adminData.products;
+  res.render("shop", { prods: products, shopTitle: "Shop" }); // render template
 });
 
 module.exports = router;
