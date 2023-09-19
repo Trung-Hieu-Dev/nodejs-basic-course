9a9867c13bc34d7b3e62bd143263c8a24728dffa
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-09-19 16:29:23 +0700

    Working with EJS Template

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-const expressHbs = require("express-handlebars");
 
 const rootDir = require("./utils/path");
 
@@ -12,15 +11,7 @@ const shopRoutes = require("./routes/shop");
 const app = express();
 
 // template engine configuration
-app.engine(
-  "hbs",
-  expressHbs({
-    layoutsDir: "views/layouts/",
-    defaultLayout: "main-layout",
-    extname: "hbs",
-  })
-);
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 // parse body
