0ee4467f9934943e8055cd3c46b79696e6a63dbe
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-09-19 17:40:27 +0700

    Refactor code

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,12 @@
-const path = require("path");
 const express = require("express");
 
 const router = express.Router();
-const rootDir = require("../utils/path");
 
 // data
 const products = [];
 
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  // res.sendFile(path.join(rootDir, "views", "add-product.html"));
   res.render("add-product", {
     path: "/admin/add-product",
     pageTitle: "Add Product",
--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,9 @@
 const express = require("express");
-const path = require("path");
 const adminData = require("./admin");
 
 const router = express.Router();
-const rootDir = require("../utils/path");
 
 router.get("/", (req, res, next) => {
-  // console.log(adminData.products);
-  // res.sendFile(path.join(rootDir, "views", "shop.html"));
-
   const products = adminData.products;
   res.render("shop", {
     prods: products,
